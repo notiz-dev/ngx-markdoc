c6bc4e53b17abe5361710c9518f012071e8f40f5
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-11-15 21:48:18 +0100

    scroll smooth and anchor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+      }),
+    ),
+  ],
 };
